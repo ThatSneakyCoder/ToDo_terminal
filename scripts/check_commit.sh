17b9a4cd62e5ac4d30083c7dfeb0799197794973
shubh220922 <shubh22.srivastava@gmail.com>
2023-09-27 15:23:11 +0530

    (commit 15) setup commit message checker

--- scripts/check_commit.sh
+++ scripts/check_commit.sh
@@ -3,7 +3,7 @@
 COMMIT_MSG=$(git log -1 --pretty=%B)
 echo "Commit Message: $COMMIT_MSG"
 
-if [[ ! $COMMIT_MSG =~ ^\(commit[[:space]][0-9]+\))[[:space]].+$ ]]; then
+if [[ ! $COMMIT_MSG =~ ^\(commit[[:space]][0-9]+\)[[:space]].+$ ]]; then
   echo "Error: Commit message does not follow the correct format"
-  exit i
+  exit 1
 fi
