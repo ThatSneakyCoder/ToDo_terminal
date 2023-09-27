7a52c375798612c45f47c9191f96be057b0fc52e
shubh220922 <shubh22.srivastava@gmail.com>
2023-09-27 15:46:07 +0530

    (commit 16) commit checker code written

--- scripts/check_commit.sh
+++ scripts/check_commit.sh
@@ -3,7 +3,10 @@
 COMMIT_MSG=$(git log -1 --pretty=%B)
 echo "Commit Message: $COMMIT_MSG"
 
-if [[ ! $COMMIT_MSG =~ ^\(commit[[:space]][0-9]+\)[[:space]].+$ ]]; then
-  echo "Error: Commit message does not follow the correct format"
+echo "$COMMIT_MSG" | grep -Pq "^\(commit\s+[0-9]+\)\s+.*$"
+
+# Check the exit code of grep
+if [ $? -ne 0 ]; then
+  echo "Error: Commit message does not follow the correct format."
   exit 1
 fi
